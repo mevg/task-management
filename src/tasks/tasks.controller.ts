d519953a926424f62def8e8d663216259cc370f0
Manuel Valenzuela <Manuel_Valenzuela@jabil.com>
2022-11-04 11:48:16 -0600

    feat(task): added task model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    
-    constructor(private taskService: TasksService){
 
+    constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
     }
 }
