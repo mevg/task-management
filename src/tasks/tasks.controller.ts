7a18d4560262682d1bb7445808896193eb6dff43
Manuel Valenzuela <Manuel_Valenzuela@jabil.com>
2022-11-04 11:08:40 -0600

    feat(tasks): added tasks module

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+    
+    constructor(private taskService: TasksService){
+
+    }
+}
